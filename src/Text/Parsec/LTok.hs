3bb01c4a2062e5e64751e05ebbc410915270b890
osa1 <omeragacan@gmail.com>
2012-10-28 18:26:11 +0200

    helpers for nums, string lits, etc.

--- src/Text/Parsec/LTok.hs
+++ src/Text/Parsec/LTok.hs
@@ -11,19 +11,29 @@ import Text.Parsec hiding (satisfy)
 
 type Parser = Parsec [LTok] ()
 
-satisfy :: (Stream [LTok] m LTok) => (LTok -> Bool) -> ParsecT [LTok] u m LTok
+satisfy :: (Stream [LTok] m LTok) => (LTok -> Bool) -> ParsecT [LTok] u m LToken
 satisfy f = tokenPrim show nextPos tokeq
   where nextPos :: SourcePos -> LTok -> [LTok] -> SourcePos
         nextPos pos _ ((_, (AlexPn _ l c)):_) = setSourceColumn (setSourceLine pos l) c
         nextPos pos _ []                  = pos -- TODO: ??
 
-        tokeq :: LTok -> Maybe LTok
-        tokeq t = if f t then Just t else Nothing
+        tokeq :: LTok -> Maybe LToken
+        tokeq t = if f t then Just (fst t) else Nothing
 
-tok :: (Stream [LTok] m LTok) => LToken -> ParsecT [LTok] u m LTok
+tok :: (Stream [LTok] m LTok) => LToken -> ParsecT [LTok] u m LToken
 tok t = satisfy (\(t', _) -> t' == t) <?> show t
 
-anyIdent :: Monad m => ParsecT [LTok] u m LTok
+anyIdent :: Monad m => ParsecT [LTok] u m LToken
 anyIdent = satisfy p <?> "ident"
   where p (t, _) = case t of LTokIdent _ -> True
                              _ -> False
+
+anyNum :: Monad m => ParsecT [LTok] u m LToken
+anyNum = satisfy p <?> "number"
+    where p (t, _) = case t of LTokNum _ -> True
+                               _ -> False
+
+string :: Monad m => ParsecT [LTok] u m LToken
+string = satisfy p <?> "string"
+    where p (t, _) = case t of LTokSLit _ -> True
+                               _ -> False
