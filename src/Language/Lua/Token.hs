85c6112030c14beece34847c6255f3129ddecdba
Ömer Sinan Ağacan <omeragacan@gmail.com>
2012-12-02 00:56:40 +0200

    custom show instance for tokens

--- src/Language/Lua/Token.hs
+++ src/Language/Lua/Token.hs
@@ -56,7 +56,64 @@ data LToken = LTokPlus               -- ^+
             | LTokSLit      String   -- ^string constant
             | LTokIdent     String   -- ^identifier
             | LTokEof                -- ^end of file
-    deriving (Show, Eq)
+    deriving Eq
+
+instance Show LToken where
+    show LTokPlus          = "`+`"
+    show LTokMinus         = "`-`"
+    show LTokStar          = "`*`"
+    show LTokSlash         = "`/`"
+    show LTokPercent       = "`%`"
+    show LTokExp           = "`^`"
+    show LTokSh            = "`#`"
+    show LTokEqual         = "`=`"
+    show LTokNotequal      = "`~=`"
+    show LTokLEq           = "`<=`"
+    show LTokGEq           = "`>=`"
+    show LTokLT            = "`<`"
+    show LTokGT            = "`>`"
+    show LTokAssign        = "`=`"
+    show LTokLParen        = "`(`"
+    show LTokRParen        = "`)`"
+    show LTokLBrace        = "`{`"
+    show LTokRBrace        = "`}`"
+    show LTokLBracket      = "`[`"
+    show LTokRBracket      = "`]`"
+    show LTokDColon        = "`::`"
+    show LTokSemic         = "`;`"
+    show LTokColon         = "`:`"
+    show LTokComma         = "`,`"
+    show LTokDot           = "`.`"
+    show LTokDDot          = "`..`"
+    show LTokEllipsis      = "`...`"
+
+    show LTokAnd           = "`and`"
+    show LTokBreak         = "`break`"
+    show LTokDo            = "`do`"
+    show LTokElse          = "`else`"
+    show LTokElseIf        = "`elseif`"
+    show LTokEnd           = "`end`"
+    show LTokFalse         = "`false`"
+    show LTokFor           = "`for`"
+    show LTokFunction      = "`function`"
+    show LTokGoto          = "`goto`"
+    show LTokIf            = "`if`"
+    show LTokIn            = "`in`"
+    show LTokLocal         = "`local`"
+    show LTokNil           = "`nil`"
+    show LTokNot           = "`not`"
+    show LTokOr            = "`or`"
+    show LTokRepeat        = "`repeat`"
+    show LTokReturn        = "`return`"
+    show LTokThen          = "`then`"
+    show LTokTrue          = "`true`"
+    show LTokUntil         = "`until`"
+    show LTokWhile         = "`while`"
+
+    show (LTokNum   n)     = "number: " ++ show n
+    show (LTokSLit  s)     = "string: " ++ show s
+    show (LTokIdent i)     = "identifier: " ++ show i
+    show LTokEof           = "EOF"
 
 -- | Partial function, returns value of `LTokNum`, `LTokSLit` and `LTokIdent`.
 tokenValue :: LToken -> String
