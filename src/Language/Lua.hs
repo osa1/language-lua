d5787dea7320bd4512ebd95f9e7c4f60e57051ce
Ömer Sinan Ağacan <omeragacan@gmail.com>
2012-12-08 13:52:49 +0200

    add exported names top-level module

--- src/Language/Lua.hs
+++ src/Language/Lua.hs
@@ -1,5 +1,16 @@
-module Language.Lua where
+module Language.Lua
+  ( parseText
+  , parseFile
+  , stat
+  , exp
+  , chunk
+  , Block(..)
+  , Stat(..)
+  , Exp(..)
+  , pprint
+  ) where
 
+import Prelude hiding (exp)
 import Language.Lua.Types
 import Language.Lua.Parser
 import Language.Lua.PrettyPrinter
