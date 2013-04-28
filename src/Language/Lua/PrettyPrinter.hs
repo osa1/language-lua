27f28957bffc60579b828ce6d4a2c392cd6dbac0
osa1 <omeragacan@gmail.com>
2013-04-28 17:38:41 +0300

    ignore annotations in pretty-printer

--- src/Language/Lua/PrettyPrinter.hs
+++ src/Language/Lua/PrettyPrinter.hs
@@ -30,60 +30,63 @@ instance LPretty Bool where
     pprint True  = text "true"
     pprint False = text "false"
 
-instance LPretty Exp where
-    pprint Nil              = text "nil"
-    pprint (Bool s)         = pprint s
-    pprint (Number n)       = text n
-    pprint (String s)       = dquotes (text s)
-    pprint Vararg           = text "..."
-    pprint (EFunDef f)      = pprint f
-    pprint (PrefixExp pe)   = pprint pe
-    pprint (TableConst t)   = pprint t
-    pprint (Binop op e1 e2) = pprint e1 <+> pprint op <+> pprint e2
-    pprint (Unop op e)      = pprint op <> pprint e
+instance LPretty (Name a) where
+    pprint (Name _ s) = text s
 
-instance LPretty Var where
-    pprint (Name n)             = text n
-    pprint (Select pe e)        = pprint pe <> brackets (pprint e)
-    pprint (SelectName pe name) = group (pprint pe <$$> (char '.' <> pprint name))
+instance LPretty (Exp a) where
+    pprint (Nil _)            = text "nil"
+    pprint (Bool _ s)         = pprint s
+    pprint (Number _ n)       = text n
+    pprint (String _ s)       = dquotes (text s)
+    pprint (Vararg _)         = text "..."
+    pprint (EFunDef _ f)      = pprint f
+    pprint (PrefixExp _ pe)   = pprint pe
+    pprint (TableConst _ t)   = pprint t
+    pprint (Binop _ op e1 e2) = pprint e1 <+> pprint op <+> pprint e2
+    pprint (Unop _ op e)      = pprint op <> pprint e
 
-instance LPretty Binop where
-    pprint Add    = char '+'
-    pprint Sub    = char '-'
-    pprint Mul    = char '*'
-    pprint Div    = char '/'
-    pprint Exp    = char '^'
-    pprint Mod    = char '%'
-    pprint Concat = text ".."
-    pprint LT     = char '<'
-    pprint LTE    = text "<="
-    pprint GT     = char '>'
-    pprint GTE    = text ">="
-    pprint EQ     = text "=="
-    pprint NEQ    = text "~="
-    pprint And    = text "and"
-    pprint Or     = text "or"
+instance LPretty (Var a) where
+    pprint (VarName _ n)          = pprint n
+    pprint (Select _ pe e)        = pprint pe <> brackets (pprint e)
+    pprint (SelectName _ pe name) = group (pprint pe <$$> (char '.' <> pprint name))
 
-instance LPretty Unop where
-    pprint Neg = char '-'
-    pprint Not = text "not "
-    pprint Len = char '#'
+instance LPretty (Binop a) where
+    pprint Add{}    = char '+'
+    pprint Sub{}    = char '-'
+    pprint Mul{}    = char '*'
+    pprint Div{}    = char '/'
+    pprint Exp{}    = char '^'
+    pprint Mod{}    = char '%'
+    pprint Concat{} = text ".."
+    pprint LT{}     = char '<'
+    pprint LTE{}    = text "<="
+    pprint GT{}     = char '>'
+    pprint GTE{}    = text ">="
+    pprint EQ{}     = text "=="
+    pprint NEQ{}    = text "~="
+    pprint And{}    = text "and"
+    pprint Or{}     = text "or"
 
-instance LPretty PrefixExp where
-    pprint (PEVar var)         = pprint var
-    pprint (PEFunCall funcall) = pprint funcall
-    pprint (Paren e)           = parens (pprint e)
+instance LPretty (Unop a) where
+    pprint Neg{} = char '-'
+    pprint Not{} = text "not "
+    pprint Len{} = char '#'
 
-instance LPretty Table where
-    pprint (Table fields) = braces (nest 4 (cat (punctuate comma (map pprint fields))))
+instance LPretty (PrefixExp a) where
+    pprint (PEVar _ var)         = pprint var
+    pprint (PEFunCall _ funcall) = pprint funcall
+    pprint (Paren _ e)           = parens (pprint e)
 
-instance LPretty TableField where
-    pprint (ExpField e1 e2)    = brackets (pprint e1) <+> equals <+> pprint e2
-    pprint (NamedField name e) = pprint name <+> equals <+> pprint e
-    pprint (Field e)           = pprint e
+instance LPretty (Table a) where
+    pprint (Table _ fields) = braces (nest 4 (cat (punctuate comma (map pprint fields))))
 
-instance LPretty Block where
-    pprint (Block stats ret)
+instance LPretty (TableField a) where
+    pprint (ExpField _ e1 e2)    = brackets (pprint e1) <+> equals <+> pprint e2
+    pprint (NamedField _ name e) = pprint name <+> equals <+> pprint e
+    pprint (Field _ e)           = pprint e
+
+instance LPretty (Block a) where
+    pprint (Block _ stats ret)
         = case stats of
             [] -> ret'
             _  -> (foldr (<$>) empty (map pprint stats)) <$> ret'
@@ -91,56 +94,56 @@ instance LPretty Block where
                      Nothing -> empty
                      Just e  -> nest 2 (text "return" </> (intercalate comma (map pprint e)))
 
-instance LPretty FunName where
-    pprint (FunName name s methods) = cat (punctuate dot (map pprint $ name:s)) <> method'
+instance LPretty (FunName a) where
+    pprint (FunName _ name s methods) = cat (punctuate dot (map pprint $ name:s)) <> method'
       where method' = case methods of
-                   Nothing -> empty
-                   Just m' -> char ':' <> text m'
+                        Nothing -> empty
+                        Just m' -> char ':' <> pprint m'
 
-instance LPretty FunDef where
-    pprint (FunDef body) = pprint body
+instance LPretty (FunDef a) where
+    pprint (FunDef _ body) = pprint body
 
-instance LPretty FunBody where
+instance LPretty (FunBody a) where
     pprint funbody = pprintFunction Nothing funbody
 
-pprintFunction :: Maybe Doc -> FunBody -> Doc
-pprintFunction funname (FunBody args vararg block)
+pprintFunction :: Maybe Doc -> FunBody a -> Doc
+pprintFunction funname (FunBody _ args vararg block)
     = group (nest 4 (funhead <$> funbody) <$> end)
   where funhead = case funname of
                     Nothing -> nest 2 (text "function" </> args')
                     Just n  -> nest 2 (text "function" </> n </> args')
         args' = parens (align (cat (punctuate (comma <> space)
-                                        (map pprint (args ++ if vararg then ["..."] else [])))))
+                                        (map pprint (args ++ if vararg then [Name undefined "..."] else []))))) -- FIXME:
         funbody = pprint block
         end = text "end"
 
-instance LPretty FunCall where
-    pprint (NormalFunCall pe arg)     = group (nest 4 (pprint pe <$$> pprint arg))
-    pprint (MethodCall pe method arg) = group (nest 4 (pprint pe <$$> (colon <> text method) <$$> pprint arg))
+instance LPretty (FunCall a) where
+    pprint (NormalFunCall _ pe arg)     = group (nest 4 (pprint pe <$$> pprint arg))
+    pprint (MethodCall _ pe method arg) = group (nest 4 (pprint pe <$$> (colon <> pprint method) <$$> pprint arg))
 
-instance LPretty FunArg where
-    pprint (Args exps)   = parens (nest 4 (cat (punctuate (comma <> space) (map pprint exps))))
-    pprint (TableArg t)  = pprint t
-    pprint (StringArg s) = dquotes (text s)
+instance LPretty (FunArg a) where
+    pprint (Args _ exps)   = parens (nest 4 (cat (punctuate (comma <> space) (map pprint exps))))
+    pprint (TableArg _ t)  = pprint t
+    pprint (StringArg _ s) = dquotes (text s)
 
-instance LPretty Stat where
-    pprint (Assign names vals)
+instance LPretty (Stat a) where
+    pprint (Assign _ names vals)
         =   (intercalate comma (map pprint names))
         <+> equals
         <+> (intercalate comma (map pprint vals))
-    pprint (FunCall funcall) = pprint funcall
-    pprint (Label name)      = text "::" <> text name <> text "::"
-    pprint Break             = text "break"
-    pprint (Goto name)       = text "goto" <+> text name
-    pprint (Do block)        = group (nest 4 (text "do" <$> pprint block) <$> text "end")
-    pprint (While guard e)
+    pprint (FunCall _ funcall) = pprint funcall
+    pprint (Label _ name)      = text "::" <> pprint name <> text "::"
+    pprint (Break _)           = text "break"
+    pprint (Goto _ name)       = text "goto" <+> pprint name
+    pprint (Do _ block)        = group (nest 4 (text "do" <$> pprint block) <$> text "end")
+    pprint (While _ guard e)
         =  (nest 4 (text "while" <+> pprint guard <+> text "do"
                    </> indent 4 (pprint e)))
        </> text "end"
-    pprint (Repeat block guard)
+    pprint (Repeat _ block guard)
         = nest 4 (text "repeat" </> pprint block) </> (nest 4 (text "until" </> pprint guard))
 
-    pprint (If cases elsePart) = group (printIf cases elsePart)
+    pprint (If _ cases elsePart) = group (printIf cases elsePart)
       where printIf ((guard, block):xs) e
                 =   group (nest 4 (text "if" <+> pprint guard <+> text "then"
                         <$> pprint block))
@@ -154,25 +157,25 @@ instance LPretty Stat where
                         <$> pprint block))
                 <$> printIf' xs e
 
-    pprint (ForRange name e1 e2 e3 block)
-        =   text "for" <+> text name <> equals <> pprint e1 <> comma <> pprint e2 <> e3' <+> text "do"
+    pprint (ForRange _ name e1 e2 e3 block)
+        =   text "for" <+> pprint name <> equals <> pprint e1 <> comma <> pprint e2 <> e3' <+> text "do"
         <$> indent 4 (pprint block)
         <$> text "end"
       where e3' = case e3 of
                     Nothing -> empty
                     Just e  -> comma <> pprint e
 
-    pprint (ForIn names exps block)
+    pprint (ForIn _ names exps block)
         =   text "for" <+> (intercalate comma (map pprint names))
                 <+> text "in" <+> (intercalate comma (map pprint exps)) <+> text "do"
         <$> indent 4 (pprint block)
         <$> text "end"
 
-    pprint (FunAssign name body) = pprintFunction (Just (pprint name)) body
-    pprint (LocalFunAssign name body) = text "local" <+> pprintFunction (Just (pprint name)) body
-    pprint (LocalAssign names exps)
+    pprint (FunAssign _ name body) = pprintFunction (Just (pprint name)) body
+    pprint (LocalFunAssign _ name body) = text "local" <+> pprintFunction (Just (pprint name)) body
+    pprint (LocalAssign _ names exps)
         = text "local" <+> (intercalate comma (map pprint names)) <+> equals <+> exps'
       where exps' = case exps of
                       Nothing -> empty
                       Just es -> intercalate comma (map pprint es)
-    pprint EmptyStat = empty
+    pprint EmptyStat{} = empty
