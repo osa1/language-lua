47c4a08e7ad0d81ae77d72e7fd58108a1d0ac4c9
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-01-18 11:50:18 -0500

    simplify property tests, improve err msgs, fix a build problem

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,15 +1,14 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
-{-# LANGUAGE DeriveGeneric, FlexibleInstances, StandaloneDeriving, ViewPatterns
-             #-}
+{-# LANGUAGE DeriveGeneric, FlexibleInstances, StandaloneDeriving #-}
 
 module Main where
 
-import           Language.Lua.Annotated
-import           Language.Lua.Annotated.Lexer
+import qualified Language.Lua.Annotated          as A
+import           Language.Lua.Annotated.Lexer    (llex)
 import qualified Language.Lua.Annotated.Simplify as S
 import qualified Language.Lua.Parser             as P
 import           Language.Lua.PrettyPrinter      (pprint)
-import qualified Language.Lua.Syntax             as S
+import           Language.Lua.Syntax
 
 import qualified Text.Parsec                     as P
 
@@ -27,25 +26,31 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [stringTests, numberTests{-, genPrintParse-}]
+tests = testGroup "Tests" [unitTests, propertyTests]
 
-parseExps :: String -> String -> Either P.ParseError [Exp P.SourcePos]
-parseExps file contents = P.runParser (many exp) () file (llex contents)
+unitTests :: TestTree
+unitTests = testGroup "Unit tests" [stringTests, numberTests]
+
+propertyTests :: TestTree
+propertyTests = testGroup "Property tests" [{-genPrintParse-}]
+
+parseExps :: String -> String -> Either P.ParseError [A.Exp P.SourcePos]
+parseExps file contents = P.runParser (many A.exp) () file (llex contents)
 
 stringTests :: TestTree
 stringTests = testGroup "String tests"
     [ testCase
-        "Strings from 5.2.2 reference manual"
+        "Equal strings from 5.2.2 reference manual"
         (do let file = "tests/strings"
             contents <- readFile file
             case parseExps file contents of
               Left parseErr -> assertFailure (show parseErr)
               Right exps -> do
                 assertBool "Wrong number of strings parsed" (length exps == 5)
-                assertEqTrans $ map (fmap (const ())) exps)
+                assertEqTrans $ map S.sExp exps)
     ]
   where
-    assertEqTrans :: [Exp ()] -> Assertion
+    assertEqTrans :: [Exp] -> Assertion
     assertEqTrans [] = return ()
     assertEqTrans [_] = return ()
     assertEqTrans (a : b : rest) = do
@@ -62,33 +67,32 @@ numberTests = testGroup "Number tests"
               Left parseErr -> assertFailure (show parseErr)
               Right exps -> do
                 assertBool "Wrong number of numbers parsed" (length exps == 9)
-                forM_ exps assertNumber)
+                forM_ exps (assertNumber . S.sExp))
     ]
   where
-    assertNumber :: Show a => Exp a -> Assertion
+    assertNumber :: Exp -> Assertion
     assertNumber Number{} = return ()
     assertNumber nan      = assertFailure ("Not a number: " ++ show nan)
 
 genPrintParse :: TestTree
 genPrintParse =
     localOption (QuickCheckTests 10)
-  . localOption (mkTimeout 1000)
+  . localOption (mkTimeout 100000)
   . localOption (QuickCheckMaxSize 2)
   $ testGroup "Generate-Print-Parse" [ testProperty "forall l, (parse . pprint) l = l" prop ]
   where
     prop :: Property
-    -- TODO: use forallShrink
     prop = forAll arbitrary printAndParseEq
 
-    printAndParseEq :: Block () -> Property
-    printAndParseEq (S.sBlock -> b) = (P.parseText P.chunk . show . pprint) b === Right b
+    printAndParseEq :: Block -> Property
+    printAndParseEq b = Right b === (P.parseText P.chunk . show . pprint) b
 
 instance Eq P.ParseError where
     _ == _ = False
 
 -- * Arbitrary instances
 
-newtype LuaString = LuaString { unwrapLuaString :: String } deriving (Generic)
+newtype LuaString = LuaString { unwrapLuaString :: String } -- deriving (Generic)
 
 -- FIXME: either fix this or implement separate lexer tests
 instance Arbitrary LuaString where
@@ -101,144 +105,127 @@ arbitraryLuaStringList = liftA unwrapLuaString <$> listOf1 arbitrary
 arbitraryLuaString :: Gen String
 arbitraryLuaString = unwrapLuaString <$> arbitrary
 
-instance Arbitrary (Name ()) where
-  arbitrary = Name () <$> arbitraryLuaString
-  shrink = recursivelyShrink
-
-instance Arbitrary (Stat ()) where
+instance Arbitrary Stat where
   arbitrary = oneof
-    [ Assign () <$> arbitrary <*> arbitrary
-    , FunCall () <$> arbitrary
-    , Label () <$> arbitrary
-    , return $ Break ()
-    , Goto () <$> arbitrary
-    , Do () <$> arbitrary
-    , While () <$> arbitrary <*> arbitrary
-    , Repeat () <$> arbitrary <*> arbitrary
-    , If () <$> listOf1 arbitrary <*> arbitrary
-    , ForRange () <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
-    , ForIn () <$> listOf1 arbitrary <*> arbitrary <*> arbitrary
-    , FunAssign () <$> arbitrary <*> arbitrary
-    , LocalFunAssign () <$> arbitrary <*> arbitrary
-    , LocalAssign () <$> listOf1 arbitrary <*> arbitrary
+    [ Assign <$> arbitrary <*> arbitrary
+    , FunCall <$> arbitrary
+    , Label <$> arbitrary
+    , return Break
+    , Goto <$> arbitrary
+    , Do <$> arbitrary
+    , While <$> arbitrary <*> arbitrary
+    , Repeat <$> arbitrary <*> arbitrary
+    , If <$> listOf1 arbitrary <*> arbitrary
+    , ForRange <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+    , ForIn <$> listOf1 arbitrary <*> arbitrary <*> arbitrary
+    , FunAssign <$> arbitrary <*> arbitrary
+    , LocalFunAssign <$> arbitrary <*> arbitrary
+    , LocalAssign <$> listOf1 arbitrary <*> arbitrary
     -- Don't generate EmptyState - it's not printed by pretty-printer
     -- , return $ EmptyStat ()
     ]
   shrink = recursivelyShrink
 
-instance Arbitrary (Exp ()) where
+instance Arbitrary Exp where
   arbitrary = oneof
-    [ return $ Nil ()
-    , Bool () <$> arbitrary
-    , Number () <$> listOf1 (elements ['0'..'9']) -- TODO: implement number lexer tests
-    , String () <$> arbitraryLuaString
-    , return $ Vararg ()
-    , EFunDef () <$> arbitrary
-    , PrefixExp () <$> arbitrary
-    , TableConst () <$> arbitrary
-    , Binop () <$> arbitrary <*> arbitrary <*> arbitrary
-    , Unop () <$> arbitrary <*> expNotUnop
+    [ return Nil
+    , Bool <$> arbitrary
+    , Number <$> listOf1 (elements ['0'..'9']) -- TODO: implement number lexer tests
+    , String <$> arbitraryLuaString
+    , return Vararg
+    , EFunDef <$> arbitrary
+    , PrefixExp <$> arbitrary
+    , TableConst <$> arbitrary
+    , Binop <$> arbitrary <*> arbitrary <*> arbitrary
+    , Unop <$> arbitrary <*> expNotUnop
     ]
   shrink = recursivelyShrink
 
 -- | Any expression except Unop. (see #2)
-expNotUnop :: Gen (Exp ())
+expNotUnop :: Gen Exp
 expNotUnop = suchThat arbitrary notUnop
   where
-    notUnop :: Exp () -> Bool
+    notUnop :: Exp -> Bool
     notUnop Unop{} = False
     notUnop _      = True
 
-instance Arbitrary (Var ()) where
+instance Arbitrary Var where
   arbitrary = oneof
-    [ VarName () <$> arbitrary
-    , Select () <$> arbitrary <*> arbitrary
-    , SelectName () <$> arbitrary <*> arbitrary
+    [ VarName <$> arbitrary
+    , Select <$> arbitrary <*> arbitrary
+    , SelectName <$> arbitrary <*> arbitrary
     ]
   shrink = recursivelyShrink
 
-instance Arbitrary (Binop ()) where
-  arbitrary = oneof $ (return <$>
-    map ($ ()) [Add, Sub, Mul, Div, Exp, Mod, Concat, LT, LTE, GT, GTE, EQ, NEQ, And, Or])
+instance Arbitrary Binop where
+  arbitrary = oneof $
+    map return [Add, Sub, Mul, Div, Exp, Mod, Concat, LT, LTE, GT, GTE, EQ, NEQ, And, Or]
   shrink = recursivelyShrink
 
-instance Arbitrary (Unop ()) where
+instance Arbitrary Unop where
   arbitrary = oneof
-    [ return $ Neg ()
-    , return $ Not ()
-    , return $ Len ()
+    [ return Neg
+    , return Not
+    , return Len
     ]
   shrink = recursivelyShrink
 
-instance Arbitrary (PrefixExp ()) where
+instance Arbitrary PrefixExp where
   arbitrary = oneof
-    [ PEVar () <$> arbitrary
-    , PEFunCall () <$> arbitrary
-    , Paren () <$> arbitrary
+    [ PEVar <$> arbitrary
+    , PEFunCall <$> arbitrary
+    , Paren <$> arbitrary
     ]
   shrink = recursivelyShrink
 
-instance Arbitrary (Table ()) where
-  arbitrary = Table () <$> arbitrary
-  shrink = recursivelyShrink
-
-instance Arbitrary (TableField ()) where
+instance Arbitrary TableField where
   arbitrary = oneof
-    [ ExpField () <$> arbitrary <*> arbitrary
-    , NamedField () <$> arbitrary <*> arbitrary
-    , Field () <$> arbitrary
+    [ ExpField <$> arbitrary <*> arbitrary
+    , NamedField <$> arbitrary <*> arbitrary
+    , Field <$> arbitrary
     ]
   shrink = recursivelyShrink
 
-instance Arbitrary (Block ()) where
-  arbitrary = Block () <$> arbitrary
-                       <*> suchThat arbitrary (\a -> case a of
-                                                       Nothing -> True
-                                                       Just l -> not (null l))
+instance Arbitrary Block where
+  arbitrary = Block <$> arbitrary
+                    <*> suchThat arbitrary (maybe True (not . null))
   shrink = recursivelyShrink
 
-instance Arbitrary (FunName ()) where
-  arbitrary = FunName () <$> arbitrary <*> listOf arbitrary <*> arbitrary
+instance Arbitrary FunName where
+  arbitrary = FunName <$> arbitrary <*> listOf arbitrary <*> arbitrary
   shrink = recursivelyShrink
 
-instance Arbitrary (FunDef ()) where
-  arbitrary = FunDef () <$> arbitrary
+instance Arbitrary FunBody where
+  arbitrary = FunBody <$> listOf1 arbitrary <*> arbitrary <*> arbitrary
   shrink = recursivelyShrink
 
-instance Arbitrary (FunBody ()) where
-  arbitrary = FunBody () <$> listOf1 arbitrary <*> arbitrary <*> arbitrary
-  shrink = recursivelyShrink
-
-instance Arbitrary (FunCall ()) where
+instance Arbitrary FunCall where
   arbitrary = oneof
-    [ NormalFunCall () <$> arbitrary <*> arbitrary
-    , MethodCall () <$> arbitrary <*> arbitrary <*> arbitrary
+    [ NormalFunCall <$> arbitrary <*> arbitrary
+    , MethodCall <$> arbitrary <*> arbitrary <*> arbitrary
     ]
   shrink = recursivelyShrink
 
-instance Arbitrary (FunArg ()) where
+instance Arbitrary FunArg where
   arbitrary = oneof
-    [ Args () <$> arbitrary
-    , TableArg () <$> arbitrary
-    , StringArg () <$> arbitrary
+    [ Args <$> arbitrary
+    , TableArg <$> arbitrary
+    , StringArg <$> arbitrary
     ]
   shrink = recursivelyShrink
 
 -- * Generic instances
 
-deriving instance Generic (Name a)
-deriving instance Generic (Stat a)
-deriving instance Generic (Exp a)
-deriving instance Generic (Var a)
-deriving instance Generic (Binop a)
-deriving instance Generic (Unop a)
-deriving instance Generic (PrefixExp a)
-deriving instance Generic (Table a)
-deriving instance Generic (TableField a)
-deriving instance Generic (Block a)
-deriving instance Generic (FunName a)
-deriving instance Generic (FunDef a)
-deriving instance Generic (FunBody a)
-deriving instance Generic (FunCall a)
-deriving instance Generic (FunArg a)
-
+deriving instance Generic LuaString
+deriving instance Generic Stat
+deriving instance Generic Exp
+deriving instance Generic Var
+deriving instance Generic Binop
+deriving instance Generic Unop
+deriving instance Generic PrefixExp
+deriving instance Generic TableField
+deriving instance Generic Block
+deriving instance Generic FunName
+deriving instance Generic FunBody
+deriving instance Generic FunCall
+deriving instance Generic FunArg
